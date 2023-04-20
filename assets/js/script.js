e284fa1fa1bad38392f655d0c4e82f86ff6394f8
A Bevers <alisabevers@gmail.com>
2023-04-20 14:43:24 -0400

    labeled the nutrition facts

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,9 @@ document.addEventListener("click", function(event) {
     }
 })
 
-
+// fetch call for ingredients and instructions
 function recipeAPI(userInput) {
     var recipeQueryURL = "https://api.api-ninjas.com/v1/recipe?query=" + userInput;
-    // fetch call for ingredients and instructions
 fetch (recipeQueryURL, {
     headers: {'X-Api-Key': APIKey}
 })
@@ -55,41 +54,29 @@ fetch (recipeQueryURL, {
         trigger: 'focus'
       })
       popover();
-
     console.log(data[0].ingredients);
     console.log(data[0].instructions);
 });
 });
 };
-
+            
+// fetch call for nutritional value of the selected meal
 function nutritionAPI(userInput) {
     var nutritionQueryURL = "https://api.api-ninjas.com/v1/nutrition?query=" + userInput;
-    // fetch call for nutritional value of the selected meal
-fetch (nutritionQueryURL, {
-    headers: {'X-Api-Key': APIKey}
-})
-.then (function (response) {
-    return response.json()
-.then (function (nutrition) {
-    for (var i = 0; i , i < nutrition.length; i++) {
-        var nutrish = document.createElement("p");
-        nutrish.textContent = `${nutrition[i].calories} ${nutrition[i].carbohydrates_total_g} ${nutrition[i].fat_total_g} ${nutrition[i].protein_g} ${nutrition[i].serving_size_g}`;
-        // mealBtn.className = "list-group-item btn btn-outline-secondary col-6";
-        // mealBtn.setAttribute("data-bs-custom-class", "custom-popup");
-        // nutrish.setAttribute("data-bs-toggle", "popover");
-        // mealBtn.setAttribute("data-bs-title", data[i].title);
-        // nutrish.setAttribute("data-bs-content", popUpContent2);
-        // mealBtn.setAttribute("data-bs-trigger", "focus");
-        nutritionEl.appendChild(nutrish);
-    }
-    console.log(nutrition);
-    // console.log(nutrition[i].calories);
-    // console.log(nutrition[i].carbohydrates_total_g);
-    // console.log(nutrition[i].fat_total_g);
-    // console.log(nutrition[i].protein_g);
-    // console.log(nutrition[i].serving_size_g);
-})
-});
+    fetch (nutritionQueryURL, {
+        headers: {'X-Api-Key': APIKey}
+    })
+    .then (function (response) {
+        return response.json()
+    .then (function (nutrition) {
+        for (var i = 0; i , i < nutrition.length; i++) {
+            var nutrish = document.createElement("p");
+            nutrish.textContent = `${nutrition[i].name} Calories: ${nutrition[i].calories} Carbs: ${nutrition[i].carbohydrates_total_g}grams Total Fat: ${nutrition[i].fat_total_g}grams Protein: ${nutrition[i].protein_g} Serving Size: ${nutrition[i].serving_size_g}grams`;
+            nutritionEl.appendChild(nutrish);
+        }
+        console.log(nutrition);
+    })
+    });
 }
 
 
@@ -101,5 +88,4 @@ searchbtn.addEventListener('click', function(event){
  var userInput=searchInput.value;
  console.log(userInput);
  recipeAPI(userInput);
-//  nutritionAPI(userInput);
 });
